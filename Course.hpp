70943d6008448a15f036a4c4d691ffd8070161e4
Romina Pouya <148919045+rominapy@users.noreply.github.com>
2024-05-03 10:44:58 -0700

    Add files via upload

---
+++ Course.hpp
@@ -0,0 +1,22 @@
+#include "Major.hpp"
+ class Course{
+
+    private:
+         int course_id;
+        string course_name;
+         float course_unit;
+
+     public:
+        // Constructor to initialize a Course object
+        Course(int id, string name, float unit) : course_id(id), course_name(name), course_unit(unit) {}
+
+ 
+ /*
+        Constructor: To initialize the Course object with default or given values.
+        getCourseName(): To get the name of the course.
+        getCourseId(): To get the ID of the course.
+        getCourseUnit(): To get the number of units the course is worth.
+
+ */
+
+ };
