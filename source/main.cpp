c8ae7cd7b37a10141951e7a54af83606b49eb013
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2024-05-08 18:29:55 -0700

    wrote a script for reading CSV

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,59 @@
-#include "Course.hpp"
-#include "Major.hpp"
-#include "University.hpp"
-#include <csv2/csv2.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello from counseling app!\n";
+    cout << "Hello from counseling app!\n";
+    
+    fstream fin;
+    fin.open("/Users/kavehz98/Documents/GitHub/CS Club/spring2024-final/data/reportcard.csv", ios::in);
+
+    int rollnum = 0;
+    int roll2 = 0;
+    int count = 0;
+
+    cout << "Enter the roll number of the student to display details: ";
+    cin >> rollnum;
+
+    vector<string> row;
+
+    string line;
+    string word;
+
+    while (getline(fin, line)) // Corrected to read the entire line
+    {
+        row.clear();
+        stringstream s(line); // Use stringstream to parse the line
+
+        while (getline(s, word, ','))
+        {
+            row.push_back(word);
+        }
+
+        roll2 = stoi(row[0]);
+
+        if (roll2 == rollnum)
+        {
+            count = 1;
+            cout << "Details of Roll " << row[0] << " : \n"; 
+            cout << "Name: " << row[1] << "\n"; 
+            cout << "Score 1: " << row[2] << "\n"; 
+            cout << "Score 2: " << row[3] << "\n"; 
+            cout << "Score 3: " << row[4] << "\n"; 
+            cout << "Score 4: " << row[5] << "\n"; 
+            break;
+        }
+    }
+
+    if (count == 0)
+    {
+        cout << "Record not found!" << endl;
+    }
+
+    return 0;
 }
