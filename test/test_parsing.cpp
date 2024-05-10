6d117bd70495fa97b4b8dbefa0420f6acc6dc96d
Matthew Reese <awesomepop012@hotmail.com>
2024-05-10 14:30:04 -0700

    Update tests.

--- test/test_parsing.cpp
+++ test/test_parsing.cpp
@@ -10,8 +10,11 @@ TEST_CASE("Basic string split cases", "[split]")
 {
     using vs = std::vector<std::string_view>;
     REQUIRE(split("123,test,$Er4", ',') == vs{"123", "test", "$Er4"});
+
     REQUIRE(split("ABC&DEF&GHI", '&') == vs{"ABC", "DEF", "GHI"});
+
     REQUIRE(split("ABC&DEF\n&GHI", '&') == vs{"ABC", "DEF"});
+
     REQUIRE(split("CS1A&CS1B|MATH 30A&PSYC 101", '&', '|') == vs{"CS1A", "CS1B"});
 
     auto expected = vs{"Computer Science", "3.00", "CS1A&CS1B&CS1C|CS1D", "MATH3A", "", "", ""};
@@ -24,9 +27,11 @@ TEST_CASE("Requirement statement parsing", "[parse_req]")
     using CC = ConjunctiveCourses;
 
     REQUIRE(parse_req("A&B&C") == DR{CC{Course("A"), Course("B"), Course("C")}});
+
     REQUIRE(parse_req("A|B|C") == DR{CC{Course("A")}, CC{Course("B")}, CC{Course("C")}});
-    REQUIRE(parse_req("A&B|C&D|E&F") ==
-            DR{CC{Course("A"), Course("B")}, CC{Course("C"), Course("D")}, CC{Course("E"), Course("F")}});
+
+    auto expected = DR{CC{Course("A"), Course("B")}, CC{Course("C"), Course("D")}, CC{Course("E"), Course("F")}};
+    REQUIRE(parse_req("A&B|C&D|E&F") == expected);
 }
 
 TEST_CASE("Major specification csv parsing", "[line_spec_to_major]")
